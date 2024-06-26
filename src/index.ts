5b0ec45e3794b8b0b8d11fbcd44126cd99c9a664
Yann <yann.dardot@gmail.com>
2024-06-26 19:47:58 +0200

    fix leave early

--- src/index.ts
+++ src/index.ts
@@ -30,16 +30,16 @@ client.on("interactionCreate", async (interaction) => {
       const { commandName } = interaction;
       if (commands[commandName as keyof typeof commands]) {
         await commands[commandName as keyof typeof commands].execute(interaction);
+        return;
       }
-      return;
     }
   
     if (interaction.isButton()) {
       const { customId } = interaction;
       if (buttons[customId as keyof typeof buttons]) {
         await buttons[customId as keyof typeof buttons].execute(interaction);
+        return;
       }
-      return;
     }
   
     if (interaction.isAutocomplete()) {
@@ -47,6 +47,7 @@ client.on("interactionCreate", async (interaction) => {
       const command = commands[commandName as keyof typeof commands];
       if (command && 'autocomplete' in command && typeof command.autocomplete == 'function'){
         await command.autocomplete(interaction);
+        return;
       }
     }
 
@@ -55,12 +56,13 @@ client.on("interactionCreate", async (interaction) => {
       const command = commands[customId as keyof typeof commands];
       if (command && 'selectString' in command && typeof command.selectString == 'function'){
         await command.selectString(interaction);
+        return;
       }
       const customIdSplit = customId.split('.')[0];
       const commandSplited = commands[customIdSplit as keyof typeof commands];
-      console.log(customIdSplit, commandSplited);
       if (commandSplited && 'selectString' in commandSplited && typeof commandSplited.selectString == 'function'){
         await commandSplited.selectString(interaction);
+        return;
       }
     }
 
@@ -69,12 +71,13 @@ client.on("interactionCreate", async (interaction) => {
       const command = commands[customId as keyof typeof commands];
       if (command && 'selectRole' in command && typeof command.selectRole == 'function'){
         await command.selectRole(interaction);
+        return;
       }
       const customIdSplit = customId.split('.')[0];
       const commandSplited = commands[customIdSplit as keyof typeof commands];
-      console.log(customIdSplit, commandSplited);
       if (commandSplited && 'selectRole' in commandSplited && typeof commandSplited.selectRole == 'function'){
         await commandSplited.selectRole(interaction);
+        return;
       }
     }
 
@@ -83,6 +86,7 @@ client.on("interactionCreate", async (interaction) => {
       const modal = modals[customId as keyof typeof modals];
       if (modal && 'submit' in modal && typeof modal.submit == 'function'){
         await modal.submit(interaction);
+        return;
       }
     }
   } catch (error) {
